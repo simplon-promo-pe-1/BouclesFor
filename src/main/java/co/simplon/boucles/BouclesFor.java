c08cc02837a5f48f17b30561429165a40e534d85
Eric SIBER <eric.siber@gmail.com>
2017-01-04 10:21:40 +0100

    2 premiers tests de BoucleForMethodeIterativeTest

--- src/main/java/co/simplon/boucles/BouclesFor.java
+++ src/main/java/co/simplon/boucles/BouclesFor.java
@@ -1,17 +1,32 @@
 package co.simplon.boucles;
 
 public class BouclesFor {
+	// ceci est une constante de la classe
 	public static final int MAX_SUPPORTED = 100;
 
+	// des attributs d'instance qui permettent de définir un entier de début et
+	// un entier de fin
 	private Integer debut = null;
 	private Integer fin = null;
+	
+	private final int tailleTableau;
 
+	/**
+	 * Constructeur par défaut
+	 */
 	public BouclesFor() {
+		this.tailleTableau = 0;
 	}
 
+	/**
+	 * Constructeur avec bornes
+	 * @param debut Borne de début
+	 * @param fin Borne de fin
+	 */
 	public BouclesFor(int debut, int fin) {
 		this.debut = debut;
 		this.fin = fin;
+		tailleTableau = this.fin - this.debut + 1;
 	}
 
 	public int[] construireTableau() {
@@ -23,7 +38,30 @@ public class BouclesFor {
 	}
 
 	public int[] construireTableauInverse() {
-		return null;
+		int[] tableauInverse = new int[tailleTableau];
+		
+		int valeur = this.fin;
+		for (int index=0 ; index < tailleTableau ; index++, valeur--) {
+			tableauInverse[index] = valeur;
+		}
+		
+		return tableauInverse;
+	}
+	
+	/**
+	 * Solution reposant sur le tableau en sens croissant
+	 * @return
+	 */
+	public int[] construireTableauInverseSolutionAlternative() {
+		int[] tableau = construireTableau();
+		int[] tableauInverse = new int[tailleTableau];
+		
+		int j = tailleTableau -1;
+		for (int i = 0 ; i < tailleTableau ; i++, j--) {
+			tableauInverse[i] = tableau[j];
+		}
+		
+		return tableauInverse;
 	}
 
 	public int[] construireTableauEntiersPairs() {
@@ -31,7 +69,14 @@ public class BouclesFor {
 	}
 
 	private int[] construireTableauAvecBornes() {
-		return null;
+		int[] tableau = new int[tailleTableau];
+
+		int index = 0;
+		for (int i = this.debut ; i <= this.fin ; i++) {
+			tableau[index++] = i;
+		}
+		
+		return tableau;
 	}
 
 	private int[] construireTableauSansBornes() {
@@ -49,7 +94,7 @@ public class BouclesFor {
 	public int[] construireTableauAvecWhile() {
 		return null;
 	}
-	
+
 	public int[] construireTableauEntiersPairsAvecWhile() {
 		return null;
 	}
